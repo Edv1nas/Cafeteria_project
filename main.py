c34f9ddb726721386afd558b8f9a08ec7cf39ea0
Edvinas Afarjancas <eafarjancas@gmail.com>
2023-05-04 17:52:54 +0300

    updated classes

--- main.py
+++ main.py
@@ -1,111 +1,146 @@
-import datetime
+from dataclasses import dataclass
 from typing import List
 
 
+@dataclass
 class Table:
-    def __init__(self, table_number: int, capacity: int,) -> None:
-        self.table_number: int = table_number
-        self.capacity: int = capacity
-        self.reserved_by: str = None
+    table_name: str
+    table_number: int
+    table_seats: int
 
-    def is_free(self):
-        return self.reserved_by is None
-
-    def reserve(self, name: str):
-        self.reserved_by = name
-
-    def get_status(self) -> str:
-        if self.is_free():
-            return f"Table {self.table_number} - [{ self.capacity} ppl] - free"
-        else:
-            return f"Table {self.table_number} - [{ self.capacity} ppl] - reserved by {self.reserved_by}, {time}."
+    def get_table_number(self) -> int:
+        return self.table_number
 
 
-class Reservation:
-    def __init__(self, name: str, time: datetime) -> None:
-        self.name = name
-        self.time = time
+@dataclass
+class Reservations:
+    reserved_by: str
+    table: Table
+    reservation_time: str
 
 
-class Meniu:
+@dataclass
+class Menu:
     pass
 
 
+@dataclass
 class Order:
     pass
 
 
+@dataclass
 class Cafeteria:
-    def __init__(self) -> List:
-        self.tables = [
-            Table(1, 2),
-            Table(2, 2),
-            Table(3, 4),
-            Table(4, 4),
-            Table(5, 6),
-            Table(6, 6)
-        ]
+    reservation_list = []
 
-        self.reservations: List = []
+    def get_table_by_number(self, table_number: int) -> Table:
+        for table in self.tables_list:
+            if table.get_table_number() == table_number:
+                return table
+        return None
 
-    def get_free_tables(self) -> List:
-        return [table for table in self.tables if table.is_free()]
+    def get_tables_list(self):
+        return self.tables_list
 
-    def show_table_status(self) -> None:
-        free_tables = self.get_free_tables()
-        occupied_tables = [
-            table for table in self.tables if not table.is_free()]
+    def reserve_table(
+        self, reserved_by: str, table_number: int, reservation_time: str
+    ) -> bool:
+        table_object = self.get_table_by_number(table_number)
+        if table_object is None:
+            return False
 
-        print("Free tables:")
-        for table in free_tables:
-            print(table.get_status())
+        for reservarion in self.reservation_list:
+            if reservarion.table.get_table_number() == table_number:
+                return False
 
-        print("\nOccupied tables:")
-        for table in occupied_tables:
-            print(table.get_status())
+        self.reservation_list.append(
+            Reservations(reserved_by, table_object, reservation_time)
+        )
+        return True
 
-    def reserve_table(self, name: str, capacity: int, time: datetime) -> None:
-        available_tables = [table for table in self.tables if table.is_free(
-        ) and table.capacity >= capacity]
+    def get_reservation_info(self, table_obj: Table) -> Reservations:
+        for reserv in self.reservation_list:
+            if reserv.table == table_obj:
+                return reserv
+        return None
 
-        if len(available_tables) == 0:
-            print("Sorry, there are no available tables.")
-            return
+    def get_free_tables_by_seats(self, seats: int):
+        free_tables = []
+        for table in self.tables_list:
+            if table.table_seats >= seats:
+                if self.get_reservation_info(table) is None:
+                    free_tables.append(table)
+        return free_tables
 
-        print("Available tables:")
-        for _, table in enumerate(available_tables):
-            print(f"{_+1}. {table.get_status()}")
+    def get_reservations(self) -> List[Reservations]:
 
-        choice = int(input("Enter table number to reserve: "))
-        table = available_tables[choice]
-        table.reserve(name)
-        reservation = Reservation(name, time)
-        self.reservations.append(reservation)
-        print(f"Table {table.table_number} reserved for {name} at {time}.")
+        return self.reservation_list
+
+    tables_list = [
+        Table("Single", table_number=1, table_seats=1),
+        Table("Single", table_number=2, table_seats=1),
+        Table("Single", table_number=3, table_seats=1),
+        Table("Double", table_number=4, table_seats=2),
+        Table("Double", table_number=5, table_seats=2),
+        Table("Double", table_number=6, table_seats=2),
+        Table("Family", table_number=7, table_seats=6),
+        Table("Family", table_number=8, table_seats=6),
+        Table("Family", table_number=9, table_seats=6),
+    ]
 
 
-cafeteria = Cafeteria()
+if __name__ == "__main__":
+    cafeteria = Cafeteria()
 
-print("Welcome to our Cafeteria!")
-name = input("Please enter your name: ")
+    print("Welcome to our Cafeteria!")
+    name = input("Please enter your name: ")
 
+    while True:
+        print("\nWhat would you like to do?")
+        print("1. Check my reservation")
+        print("2. Reserve a table")
+        print("3. View all table status")
+        print("4. Quit")
 
-while True:
-    print("\nWhat would you like to do?")
-    print("1. Reserve a table")
-    print("2. View table status")
-    print("3. Quit")
+        choice = int(input("Enter your choice (1-4): "))
+        if choice == 1:
+            print("LIST OF RESERVATIONS: ")
+            for reservation in cafeteria.get_reservations():
+                print(
+                    f"Reserved by: {reservation.reserved_by}, time: {reservation.reservation_time}, table number: {reservation.table.get_table_number()}"
+                )
 
-    choice = int(input("Enter your choice (1-3): "))
+        elif choice == 2:
+            seats = int(input("How many people in your group? "))
+            if seats >= 7:
+                print(
+                    "\nMax number of ppl per table is 6, please try to reserve table again or reserve to separate tables.")
+                continue
 
-    if choice == 1:
-        capacity = int(input("How many people in your group? "))
-        time = input(
-            "What time would you like to reserve? Enter time in HH:MM ")
-        cafeteria.reserve_table(name, capacity, time)
+            time = input(
+                "What time would you like to reserve? Enter time in HH:MM ")
+            for free_table in cafeteria.get_free_tables_by_seats(seats=seats):
+                print(
+                    f"Table name: {free_table.table_name}, Table number: {free_table.table_number}, Seats: {free_table.table_seats}"
+                )
+            selected_table = int(input("Selected table number: "))
+            if cafeteria.reserve_table(name, selected_table, time):
+                print(
+                    f"Table {selected_table} - [{seats} ppl] - Reserved by {name}, {time}")
+            else:
+                print("no")
 
-    elif choice == 2:
-        cafeteria.show_table_status()
+        elif choice == 3:
+            for rest_table in cafeteria.get_tables_list():
+                info = cafeteria.get_reservation_info(rest_table)
+                if info is None:
+                    print(
+                        f"Table name: {rest_table.table_name}, Table number: {rest_table.table_number}, Seats: {rest_table.table_seats}"
+                    )
+                else:
+                    print(
+                        f"Table name: {rest_table.table_name}, Table number: {rest_table.table_number}, Seats: {rest_table.table_seats} [Reserved by: {info.reserved_by}]"
+                    )
 
-    elif choice == 3:
-        break
+        elif choice == 4:
+            break
